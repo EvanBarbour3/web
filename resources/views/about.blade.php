a9b35787d3538cb566560dca3d40b1b6fcfaa4be
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2016-03-15 16:57:02 +0000

    Test about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,18 +1,20 @@
 @extends('skeleton')
-@extends('navigation')
 @section('content')
-<body>
-	<div class="container">
-		<div class="section no-pad">
-			<div class="row home-container">
-				<div class="col s12 m12 l12 white-text">
-					Evannnn
+	<main>
+		<div class="container">
+			<div class="section no-pad">
+				<div class="row home-container">
+					<div class="col s12 m12 l12 white-text">
+						<h1 class="home-h1">Evan Barbour</h1>
+						<h4 class="home-h4">Junior Software Developer</h4>
+					</div>
+				</div>
+				<div class="row home-default-row">
+					<div class="col s12 m12 l12">
+						<h4 class="home-h4 explore" data-hover="hi"><a href="#" class="white-text">Explore my site</a></h4>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-    <script type="text/javascript">
-        $(".button-collapse").sideNav();
-    </script>
-</body>
+	</main>
 @stop
